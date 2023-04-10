c702c375c0ebb3fad2f36bd7bc3205f1a62e4fdd
masmuhamadjamaludin <126693585+masmuhamadjamaludin@users.noreply.github.com>
2023-04-10 18:38:02 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route("/movie", methods=["POST"])
+def web_movie_post():
+    # sample_receive = request.form['sample_give']
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+    doc = {
+        'name': name_receive,
+        'address': address_receive,
+        'size': size_receive,
+    }
+    db.orders.insert_one(doc)
+    return jsonify({'msg': 'complete!'})
+
+@app.route("/movie", methods=["GET"])
+def web_movie_get():
+    orders_list = list(db.orders.find({}, {'_id': False}))
+    return jsonify({'orders': orders_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
